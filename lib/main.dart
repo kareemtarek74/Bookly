0cecf731053f595ba5299b9917b63adb7fe529fa
kareem <kareemtarek015@gmail.com>
2024-03-03 01:33:30 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+  }
+}
